2a77ec5d8b7ab7339a17f50a3b7f9308d6903c1c
LongTN <longtn@imt-soft.com>
2024-06-10 15:39:50 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+
+import 'package:android_id/android_id.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String? deviceUUID;
+
+  Future getDeviceUUID() async {
+    if (Platform.isAndroid) {
+      deviceUUID = await const AndroidId().getId();
+    } else if (Platform.isIOS) {
+      deviceUUID = (await DeviceInfoPlugin().iosInfo).identifierForVendor;
+    }
+
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getDeviceUUID();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Center(child: Text(deviceUUID ?? 'unknown')),
+    );
+  }
+}
