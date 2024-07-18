2f1da8d0a9b484b5b0868f7ba3e7edfa6151b81a
LongTN <longtn@imt-soft.com>
2024-07-18 13:51:10 +0700

    Add button full screen

--- lib/screen/youtube_page.dart
+++ lib/screen/youtube_page.dart
@@ -1,6 +1,8 @@
 import 'package:fluent_ui/fluent_ui.dart' hide ProgressBar;
 import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
+import '../widget/youtube_fullscreen.dart';
+
 class YoutubePage extends StatefulWidget {
   const YoutubePage({super.key});
 
@@ -20,13 +22,33 @@ class _YoutubePageState extends State<YoutubePage> {
     super.dispose();
   }
 
+  Future<void> _exitFullscreen() async {
+    double? currentTime = await _controller.currentTime;
+
+    if (!mounted) return;
+    currentTime = await YoutubeFullscreen(
+      videoId: '4AoFA19gbLo',
+      startSeconds: currentTime,
+    ).launch(context);
+
+    if (currentTime == null) return;
+    _controller.seekTo(seconds: currentTime);
+    _controller.exitFullScreen();
+  }
+
   @override
   Widget build(BuildContext context) {
     return YoutubePlayerScaffold(
       controller: _controller,
       builder: (context, player) {
         return ScaffoldPage.withPadding(
-          header: const PageHeader(title: Text('Youtube')),
+          header: PageHeader(
+            title: const Text('Youtube'),
+            commandBar: Button(
+              onPressed: _exitFullscreen,
+              child: const Text('Enter fullscreen'),
+            ),
+          ),
           content: SafeArea(child: player),
         );
       },
